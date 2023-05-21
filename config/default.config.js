dc85edc8e9476984a18b8fa84d7f6df7feaba997
AmaniEzz <amanymounas@gmail.com>
2023-05-22 00:05:11 +0300

    edit kubernetes configs

--- config/default.config.js
+++ config/default.config.js
@@ -1,3 +1,7 @@
+// If you were running MongoDB and Node.js on the same server, you would access MongoDB like so:
+// mongodb://localhost:27017/database-name
+// With this Kubernetes setup, that line of code would become:
+// mongodb://mongo:27017/database-name
 module.exports = {
-  dbUrl: "mongodb://localhost:27017/todoDB",
+  dbUrl: "mongodb://mongo:27017/todo-app",
 };
